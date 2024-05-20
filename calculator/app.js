211b89ee97e9ceba71c2dc82b145370c8bccb712
Max <98463490+oblikfeo@users.noreply.github.com>
2024-05-21 04:28:58 +0600

    switch

--- calculator/app.js
+++ calculator/app.js
@@ -11,15 +11,47 @@ addEventListener('click', butNum)
 function butNum(e) {
     const numValue = e.target.value
     if (numValue < 10) resultatElement.innerText += numValue
-    console.log(numValue)
 }
 
-// функция подсчета
+// функция сбора данных
 let first
 let second
 let operation
+let sum
 
 addEventListener('click', (e) => {
-    console.log()
-    console.log()
+    switch(e.target.value) {
+        case '+': 
+            if (e.target.value == "+") {
+            first = resultatElement.textContent
+            resultatElement.innerText = ''
+            sum = first + second
+            };
+        case '-':
+            if (e.target.value == "-") {
+                first = resultatElement.textContent
+                resultatElement.innerText = ''
+                operation = '-'
+            };
+        case 'x': 
+            if (e.target.value == "x") {
+            first = resultatElement.textContent
+            resultatElement.innerText = ''
+            operation = '*'
+            };
+        case '/': 
+            if (e.target.value == "/") {
+                first = resultatElement.textContent
+                resultatElement.innerText = ''
+                operation = '/'
+            };
+        case '=':
+            if (first) {
+                second = resultatElement.textContent
+                console.log(sum)
+            }
+        }
+        console.log(first)
+        console.log(second)
 })
+
