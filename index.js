88727bdc95d92aeb347d259e199e204f65c471e6
Max <98463490+oblikfeo@users.noreply.github.com>
2024-06-11 14:46:14 +0600

    add task

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function myColor() {
     let color = document.querySelector('.fon').value;
     document.body.style.backgroundColor = color;
     }
+
+// добавление тасков
+document.querySelector('.add').addEventListener('click', addTask)
+function addTask() {
+    let task = document.querySelector('.input')
+    let divActive = document.createElement('div')
+    divActive.className = "active"
+    document.querySelector('.main').appendChild(divActive)
+    divActive.innerText = task.value
+    task.value = ""
+}
+
+
